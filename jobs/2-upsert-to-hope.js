f8a87b022b1b288f49ef7b51c759da1eaaf33bbc
Taylor Downs <taylor@openfn.org>
2021-09-24 13:50:11 +0100

    elias@openfn.org's change on OpenFn.org

--- jobs/2-upsert-to-hope.js
+++ jobs/2-upsert-to-hope.js
@@ -4,10 +4,7 @@ fn(state => {
 });
 
 upsertMany(
-  'users', // the DB table
-  'email', // a DB column with a unique constraint OR a CONSTRAINT NAME
-  [
-    { name: 'one', email: 'one@openfn.org' },
-    { name: 'two', email: 'two@openfn.org' },
-  ]
+  'cases', // the DB table
+  'primero_case_id', // a DB column with a unique constraint OR a CONSTRAINT NAME
+  state => state.data.map(c => ({ first_name: c.name_first, last_name: c.name_last, gender: c.sex, dob: c.date_of_birth}))
 );
