67234befa6922f81ecbba8c31330e5dc33321f6f
Taylor Downs <taylor@openfn.org>
2021-09-24 13:33:53 +0100

    taylor@openfn.org's change on OpenFn.org

--- jobs/1-get-cases.js
+++ jobs/1-get-cases.js
@@ -1,8 +1,6 @@
 // set up version control first!
 getCases({
   remote: true,
-  last_updated_at: '2021-09-24T00:00:00.000Z',
-}, state => {
-  console.log(state.data);
-  return state;
+  // TODO: make sure date filter grabs anything updated TODAY!
+  // last_updated_at: '2021-09-24T00:00:00.000Z',
 });
