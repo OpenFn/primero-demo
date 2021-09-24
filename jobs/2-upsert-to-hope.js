d5f467b728c404148b0131d29a2957bbca04516b
Taylor Downs <taylor@openfn.org>
2021-09-24 13:38:01 +0100

    taylor@openfn.org's change on OpenFn.org

--- jobs/2-upsert-to-hope.js
+++ jobs/2-upsert-to-hope.js
@@ -1,11 +1,13 @@
 fn(state => {
   console.log(state.data);
   return state;
-})
+});
 
-// upsert(
-//   'users', // the DB table
-//   'ON CONSTRAINT users_pkey', // a DB column with a unique constraint OR a CONSTRAINT NAME
-//   { name: 'Elodie', id: 7 },
-//   { writeSql:true, execute: true }
-// );
+upsertMany(
+  'users', // the DB table
+  'email', // a DB column with a unique constraint OR a CONSTRAINT NAME
+  [
+    { name: 'one', email: 'one@openfn.org' },
+    { name: 'two', email: 'two@openfn.org' },
+  ]
+);
