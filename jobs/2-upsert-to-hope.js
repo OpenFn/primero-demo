fd0f0f2f19a8fb0105a9482ea0489321d30bd118
Taylor Downs <taylor@openfn.org>
2021-09-24 13:27:12 +0100

    taylor@openfn.org's change on OpenFn.org

--- jobs/2-upsert-to-hope.js
+++ jobs/2-upsert-to-hope.js
@@ -1,1 +1,6 @@
-// some stuff here.
+upsert(
+  'users', // the DB table
+  'ON CONSTRAINT users_pkey', // a DB column with a unique constraint OR a CONSTRAINT NAME
+  { name: 'Elodie', id: 7 },
+  { writeSql:true, execute: true }
+);
