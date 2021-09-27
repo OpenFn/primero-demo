657da48c99c1e7fd7c86e3968759cf817cbe0809
Aleksa Krolls <aleksagk@gmail.com>
2021-09-27 09:58:19 +0300

    aleksa@openfn.org's change on OpenFn.org

--- jobs/1-get-cases.js
+++ jobs/1-get-cases.js
@@ -1,4 +1,5 @@
-getCases({ 
+getCases({
+  status: 'open'
   //last_updated_at: '2021-09-27T00:00:00.000Z..' //This returns 1 case
-  last_updated_at: `${new Date().toISOString()}..` //This returns 0 cases; maybe issue with timezones?
+  //last_updated_at: `${new Date().toISOString()}..` //This returns 0 cases; maybe issue with timezones?
 });
