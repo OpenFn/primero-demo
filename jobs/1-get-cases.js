7146547873531abf2dcbee70e72a81edb1210b32
Taylor Downs <taylor@openfn.org>
2021-09-27 01:26:38 +0100

    elias@openfn.org's change on OpenFn.org

--- jobs/1-get-cases.js
+++ jobs/1-get-cases.js
@@ -1,3 +1,3 @@
 // set up version control first!
 console.log(`${new Date().toISOString()}..`)
-getCases({ remote: true, last_updated_at: new Date().toISOString() });
+getCases({ remote: true, last_updated_at: `${new Date().toISOString()}..` });
