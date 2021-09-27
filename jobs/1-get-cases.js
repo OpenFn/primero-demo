1d1d46bbd49e06cd44025d66e0289b4e1f401cd4
Aleksa Krolls <aleksagk@gmail.com>
2021-09-27 10:05:12 +0300

    aleksa@openfn.org's change on OpenFn.org

--- jobs/1-get-cases.js
+++ jobs/1-get-cases.js
@@ -1,6 +1,6 @@
 getCases({
   status: 'open',
-  region_90a5b9b: 'gambella_95177'
+  protection_concerns: `['denial_of_resources_opportunities_or_services']`,
   //last_updated_at: '2021-09-27T00:00:00.000Z..' //This returns 1 case
   //last_updated_at: `${new Date().toISOString()}..` //This returns 0 cases; maybe issue with timezones?
 });
